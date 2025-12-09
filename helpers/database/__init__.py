d6e6cf329cff163cb53ca2da927b245d98360401
Lucas Emanuel <168912467+LucasDoJava@users.noreply.github.com>
2025-12-08 23:10:52 -0300

    Update __init__.py

--- helpers/database/__init__.py
+++ helpers/database/__init__.py
@@ -1,17 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
-from helpers.application import app
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
-db.init_app(app)
-
-migrate = Migrate(app, db)
 
 
 #flask db init
