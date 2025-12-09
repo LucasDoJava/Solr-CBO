34d384bc925738a015d9faab53d3d9a4190cb39a
Lucas Emanuel <168912467+LucasDoJava@users.noreply.github.com>
2025-12-08 23:11:32 -0300

    Update __init__.py

--- helpers/application/__init__.py
+++ helpers/application/__init__.py
@@ -1,9 +1,27 @@
 from flask import Flask
 from flask_restful import Api
+from flask_migrate import Migrate
+from helpers.database import db
+from resources.resource_cbo import CBOListResource, CBODetailResource
+from resources.resource_cbo import CBOSearchResource
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:123456@localhost:5432/habits"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@db:5432/CBO"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-api = Api(app)
+    # Inicializa DB
+    db.init_app(app)
+
+    # Inicializa migrate
+    Migrate(app, db)
+
+    api = Api(app)
+
+    # Rotas
+    api.add_resource(CBOListResource, "/cbo")                    
+    api.add_resource(CBODetailResource, "/cbo/<string:codigo>") 
+    api.add_resource(CBOSearchResource, "/search") 
+
+    return app
